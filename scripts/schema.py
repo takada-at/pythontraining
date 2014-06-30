469dd5e29f29e3057c8d2c08e8ef2de2ceb69833
takada-at <takada-at@klab.com>
2014-06-30 12:27:45 +0900

    rename

--- scripts/schema.py
+++ scripts/schema.py
@@ -2,6 +2,6 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from myapp.scripts.schema import main
+from bbs.scripts.schema import main
 if __name__ == '__main__':
     main()
