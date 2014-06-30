570711194ec4d9eff504601071300fe09cf2969a
takada-at <takada-at@klab.com>
2014-06-30 14:16:41 +0900

    rename

--- scripts/run.py
+++ scripts/run.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-import myapp
-app = myapp.get_app()
+import bbs
+app = bbs.get_app()
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
